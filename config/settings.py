ea0fa9e13857879e0f297d4b4cde172af1a35557
mickamwamba <kimollomick17@gmail.com>
2025-12-08 00:21:10 -0600

    feat: Configure PostgreSQL database, add initial data, and create requirements file.

--- config/settings.py
+++ config/settings.py
@@ -131,8 +131,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'music360',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
